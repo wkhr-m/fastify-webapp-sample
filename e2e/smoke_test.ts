85dac20d3ceb7cce716ce5224adcf9204aab5239
wkhr-m <43362066+wkhr-m@users.noreply.github.com>
2025-11-11 18:16:10 +0900

    ログインのスモークテストケースを追加

--- e2e/smoke_test.ts
+++ e2e/smoke_test.ts
@@ -1,6 +1,10 @@
 Feature("スモークテスト");
 
-Scenario("example.comにアクセスする", ({ I }) => {
-  I.amOnPage("https://example.com");
-  I.see("Example Domain");
+Scenario("Webサイトを開きログインする", ({ I }) => {
+    I.amOnPage('https://fastify-webapp-sample.takuyasuemura.dev/')
+    I.click('ログインする')
+    I.fillField('ユーザー名', 'user1')
+    I.fillField('パスワード', 'super-strong-passphrase')
+    I.click('ログイン')
+    I.see('user1 さん')
 });
