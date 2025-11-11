20f385dc058584bcd5f7d9ff471329dadb08fd58
wkhr-m <43362066+wkhr-m@users.noreply.github.com>
2025-11-11 17:46:56 +0900

    E2Eテストツールのインストール

---
+++ e2e/steps.d.ts
@@ -0,0 +1,15 @@
+/// <reference types='codeceptjs' />
+type steps_file = typeof import("./steps_file");
+
+declare namespace CodeceptJS {
+  interface SupportObject {
+    I: I;
+    current: any;
+  }
+  interface Methods extends Playwright {}
+  interface I extends WithTranslation<Methods>, ReturnType<steps_file> {}
+  namespace Translation {
+    interface Actions {}
+  }
+}
+
